26e1a3be48487a6d8feea580a07cad0a94c567e0
Ezka77 <ezka.actis@gmail.com>
2019-06-05 18:14:13 +0200

    link plugin script rework
    Static names for plugins, this way it's easy to disable a plugin during
    the install process.

--- alpine/link_plugins.sh
+++ alpine/link_plugins.sh
@@ -3,10 +3,26 @@
 # link all plugins
 PACKAGES_DIR=/home/node/xen-orchestra/packages
 
+PLUGINS="xo-server-transport-xmpp \
+xo-server-transport-email \
+xo-server-backup-reports \
+xo-server-auth-github \
+xo-server-transport-slack \
+xo-server-auth-ldap \
+xo-server-auth-saml \
+xo-server-usage-report \
+xo-server-auth-google \
+xo-server-load-balancer \
+xo-server-test-plugin \
+xo-server-transport-nagios \
+xo-server-perf-alert"
+
+# Disabled
+# xo-server-cloud \
 
 cd ${PACKAGES_DIR}/xo-server/node_modules
 
-for elem in $(find $PACKAGES_DIR -maxdepth 1 -type d -name "xo-server-*"); do
-    ln -s $elem $(basename $elem)
+for elem in ${PLUGINS}; do
+    ln -s ${PACKAGES_DIR}/$elem $elem
 done;
 
